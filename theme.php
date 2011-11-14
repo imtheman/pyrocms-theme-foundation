47ca07b250f54358d8db86543ca61ca3a4dab357
Tim Asp <timothy.asp@gmail.com>
2011-11-13 18:13:42 -0800

    initial commit

---
+++ theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Foo extends Theme {
+
+    public $name = 'Theme Foo';
+    public $author = 'Tim Asp';
+    public $author_website	= 'http://timasp.com/';
+    public $website = 'http://timasp.com/';
+    public $description = 'Default theme template';
+    public $version = '1.0';
+
+}
+
+/* End of file theme.php */
